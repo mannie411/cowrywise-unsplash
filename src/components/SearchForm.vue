09c611b7cf0896d4b20f11a6d871c90f234875e3
mannie411 <imohyo@gmail.com>
2022-01-17 13:50:57 +0000

    created basic structure

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="search" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    search: String,
+  },
+})
+export default class SearchForm extends Vue {
+  search!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
