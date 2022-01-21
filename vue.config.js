a38f8de3a7ed2a1dc03b0d2b441ca288bbe0a9b7
mannie411 <imohyo@gmail.com>
2022-01-21 05:30:54 +0000

    updated vue for prod

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/cowrywise-unsplash/" : "/",
+    process.env.NODE_ENV === "production" ? "/" : "/cowrywise-unsplash/",
 };
